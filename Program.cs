349d4ec01c7894ae61a19582c85ff12533d352b5
gabriel <gabriel@DESKTOP-R93P73E>
2021-10-04 15:41:23 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DioProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
